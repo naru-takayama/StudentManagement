eaaab79681a30deb21d371ee55a29a503a11c359
narumi <l2520956161as@gmail.com>
2026-03-15 10:39:40 +0900

    Javaスタンダード第27回テスト完了

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -50,4 +50,64 @@ class StudentServiceTest {
     verify(repository,times(1)).searchStudentCourseList();
     verify(converter,times(1)).convertStudentDetails(studentList,studentCourseList);
   }
+
+  @Test
+  void 受講生詳細の検索studentIdと紐づく受講生コース情報を適切に呼び出していること(){
+
+    String studentId = "26";
+    Integer id = Integer.valueOf(studentId);
+
+    Student student = new Student();
+    student.setId(id);
+
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+
+    when(repository.searchStudent(studentId)).thenReturn(student);
+    when(repository.searchStudentCourse(id)).thenReturn(studentCourseList);
+
+    StudentDetail actual = sut.searchStudent(studentId);
+
+    verify(repository, times(1)).searchStudent(studentId);
+    verify(repository, times(1)).searchStudentCourse(id);
+
+    assertEquals(id, actual.getStudent().getId());
+  }
+
+  @Test
+  void 受講生詳細の新規登録＿リポジトリの処理が適切に呼び出せていること(){
+
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+    studentCourseList.add(studentCourse);
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentCourseList(studentCourseList);
+
+    StudentDetail actual = sut.registerStudent(studentDetail);
+
+    verify(repository, times(1)).registerStudent(student);
+    verify(repository, times(1)).registerStudentCourse(studentCourse);
+
+    assertEquals(student, actual.getStudent());
+  }
+
+  @Test
+  void 受講生詳細の更新＿指定したIdの更新処理が呼び出されていること(){
+
+    Student student = new Student();
+    student.setId(26);
+
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    sut.updateStudent(studentDetail);
+
+    verify(repository, times(1)).updateStudent(student);
+    verify(repository, times(1)).updateStudentCourse(studentCourse);
+  }
 }
