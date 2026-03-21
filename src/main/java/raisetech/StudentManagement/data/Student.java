f3f69888e1efbf28ba6e8ac282b9b918b2ec6171
narumi <l2520956161as@gmail.com>
2026-03-21 10:04:05 +0900

    Javaスタンダード第28回テスト完了

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 import jakarta.validation.constraints.Email;
@@ -11,6 +12,7 @@ import jakarta.validation.constraints.NotNull;
 @Getter
 @Setter
 public class Student {
+  @Min(value = 1, message = "1以上入力してください。")
   private Integer id;
   @NotBlank(message = "名前は必須です")
   private String fullName;
---
+++ src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
@@ -0,0 +1,139 @@
+package raisetech.StudentManagement.controller;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.service.StudentService;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebMvcTest(StudentController.class)
+
+class StudentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockitoBean
+  private StudentService service;
+
+  private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+  @Test
+  void 受講生詳細の一覧検索が実行できて空のリストが返ってくること() throws Exception {
+    when(service.searchStudentList()).thenReturn(List.of(new StudentDetail()));
+    mockMvc.perform(get("/studentList"))
+        .andExpect(status().isOk())
+        .andExpect(content().json("[{\"student\":null,\"studentCourseList\":[]}]"));
+
+    verify(service, times(1)).searchStudentList();
+  }
+
+  @Test
+  void 受講生検索が実行できること() throws Exception {
+    Student student = new Student();
+    String id = "33";
+    student.setId(Integer.valueOf(id));
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    when(service.searchStudent(id)).thenReturn(studentDetail);
+    mockMvc.perform(MockMvcRequestBuilders.get("/student/{id}", id))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).searchStudent(id);
+    assertEquals(studentDetail.getStudent().getId(), student.getId());
+  }
+
+
+  @Test
+  void 受講生の詳細の受講生で入力チェックに異常が発生しないこと() {
+    Student student = new Student();
+    student.setId(Integer.valueOf("33"));
+    student.setFullName("Ami Takayama");
+    student.setFurigana("Ami Takayama");
+    student.setNickname("Amimaru");
+    student.setEmail("Ami_takayama@example.com");
+    student.setAge(Integer.valueOf("23"));
+    student.setRegion("Miyagi Sendai");
+    student.setGender("zyosei");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+
+    assertThat(violations.size()).isEqualTo(0);
+  }
+
+  @Test
+  void 受講生の詳細の受講生でIDが1より低い数字を用いて入力してきた時に入力チェックに掛かること() {
+
+    Student student = new Student();
+    student.setId(0);
+    student.setFullName("Ami Takayama");
+    student.setFurigana("Ami Takayama");
+    student.setNickname("Amimaru");
+    student.setEmail("test@test.com");
+    student.setAge(23);
+    student.setRegion("Miyagi Sendai");
+    student.setGender("zyosei");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+    assertThat(violations.size()).isEqualTo(1);
+    assertThat(violations).extracting("message")
+        .contains("1以上入力してください。");
+  }
+
+  @Test
+  void 受講生の詳細の受講生でEmailがメール形式じゃない形式で入力してきた時に入力チェックに掛かること() {
+
+    Student student = new Student();
+    student.setId(33);
+    student.setFullName("Ami Takayama");
+    student.setFurigana("Ami Takayama");
+    student.setNickname("Amimaru");
+    student.setEmail("aaa");
+    student.setAge(23);
+    student.setRegion("Miyagi Sendai");
+    student.setGender("zyosei");
+
+    Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
+    assertThat(violations.size()).isEqualTo(1);
+    assertThat(violations).extracting("message")
+        .contains("メールアドレスの形式が正しくありません");
+  }
+
+  @Test
+  void 受講生のコース情報で終了日が過去の日付で入力してきた時に入力チェックに掛かること() {
+
+    StudentCourse studentcourse = new StudentCourse();
+    studentcourse.setId(33);
+    studentcourse.setStudentId(33);
+    studentcourse.setCourseName("AWS");
+    studentcourse.setStartDate(LocalDate.now());
+    studentcourse.setEndDate(LocalDate.now().minusDays(1));
+
+    Set<ConstraintViolation<StudentCourse>> violations = validator.validate(studentcourse);
+
+    assertThat(violations.size()).isEqualTo(1);
+    assertThat(violations).extracting("message")
+        .contains("過去の日付を入力しないでください");
+  }
+  }
