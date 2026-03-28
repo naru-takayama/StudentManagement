bd951e9aa6cd66fb29c3ad152d1e8be60c74aab6
narumi <l2520956161as@gmail.com>
2026-03-28 19:23:16 +0900

    Javaスタンダード第29回ConverterTest完成

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -1,4 +1,5 @@
 package raisetech.StudentManagement.controller.converter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,16 +7,17 @@ import org.springframework.stereotype.Component;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
+
 /**
  * 受講生詳細を受講生や受講生コース情報、もしくはその逆の返還を行うコンバーターです。
  */
 @Component
 public class StudentConverter {
+
   /**
-   * 受講生に紐づく受講生コース情報をマッピングする。
-   * 受講生コース情報は受講生に対して複数存在するのでループを回して受講生詳細情報を組み立てる。
+   * 受講生に紐づく受講生コース情報をマッピングする。 受講生コース情報は受講生に対して複数存在するのでループを回して受講生詳細情報を組み立てる。
    *
-   * @param studentList 受講生一覧
+   * @param studentList       受講生一覧
    * @param studentCourseList 受講生コース情報のリスト
    * @return 受講生詳細情報のリスト
    */
@@ -35,4 +37,5 @@ public class StudentConverter {
     });
     return studentDetails;
   }
+
 }
